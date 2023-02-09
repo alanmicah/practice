1700085ebf561ad44cf55644e9c0493696f2244c
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-02-09 12:38:14 +0000

    leftrotation

--- leftrotation.py
+++ leftrotation.py
@@ -2,21 +2,32 @@ d = 4
 n = [1,2,3,4,5]
 # Create a deep copy. Any changes made to copy doesn't reflect the original
 copyn = n.copy()
-count = 2
+count = 0
 length = len(n)
 
 print(n[0])
 
-while count < d:
-    for i in n:
-        if count+d >= len(n):
-            if (count-len(n))-d < 0:
-                print(copyn[-((count-len(n)))-d])
-                copyn[-((count+d)-len(n))] = i
-            elif (count+d-len(n))+d < len(n): 
-                copyn[(count+d-len(n))+d] = i
-        else:
-            copyn[count+d] = i
-        count += 1
+# while count < d:
+#     for i in n:
+#         if count+d >= len(n):
+#             if ((count+d) - len(n)-1) == 0:
+#                 copyn[count+d] = i
+#             else: 
+#                 copyn[(count+d) - len(n)-1] = i
+#         else:
+#             copyn[count+d] = i
+#         #     if (count-len(n))-d < 0:
+#         #         print(copyn[-((count-len(n)))-d])
+#         #         copyn[-((count+d)-len(n))] = i
+#         #     elif (count+d-len(n))+d < len(n): 
+#         #         copyn[(count+d-len(n))+d] = i
+#         # else:
+#         #     copyn[count+d] = i
+#         count += 1
 
-print(copyn)
+alist = list(n)
+b = alist[d:]+alist[:d]
+
+print(alist[:d])
+print("-----")
+print(alist[d:])
