dac2d4317191d1dfcf5219d1162b319565ecc24c
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-02-15 13:42:03 +0000

    newyearchaos self

---
+++ newyearchaos2.py
@@ -0,0 +1,24 @@
+n = [4,1,2,3,5]
+# n = [1,2,3,5,4]
+
+def nychaos(n):
+    moves = 0
+    Q = [P-1 for P in n]
+
+    print(Q)
+    # keep tracking i is the index number and P is the value
+    for i, p in enumerate(n):
+        # As the value and index should match, this checks if there is difference greater than 2
+        # This checks if a value has bribed more than 2 people
+        # if p - i > 2:
+        #     print('Too chaotic')
+        #     return
+
+        # Checks whether the value is greater than the value at the index position
+        for j in range(p):
+            if Q[j] > p:
+                moves += 1
+
+    print(moves)
+
+nychaos(n)
