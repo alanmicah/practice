7a89ce1722b3a762290d033eaca6e53fa6259c54
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-02-23 16:15:10 +0000

    set

--- twostring.py
+++ twostring.py
@@ -1,9 +1,23 @@
 s1 = 'hello'
 s2 = 'world'
 
+def twoStrings2(s1, s2):
+    # print(set(s1))
+    return 'YES' if set(s1) & set(s2) else 'NO'
+
 def twoStrings(s1, s2):
-    
+    match = 0
+    for i in s1:
+        for j in s2:
+            if i == j:
+                match += 1
+                break
+    if match > 0:
+        return 'YES'
+    else:
+        return 'NO'
+        
 
 
 
-twoStrings(s1, s2)
+print(twoStrings2(s1, s2))
