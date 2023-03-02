61f28006a3cbb1d1506955048da17f000a6ec4a3
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-03-02 14:19:42 +0000

    retry twostrings

---
+++ retry/twostrings.py
@@ -0,0 +1,19 @@
+# s1 = 'and'
+# s2 = 'art'
+
+s1 = 'world'
+s2 = 'hi'
+
+# if set(s1) & set(s2):
+#     print('YES')
+# else:
+#     print('NO')
+
+
+def two_strings(s1,s2):
+    # sets uses a hashtable
+    # hashtables typically have O(1) time complexity
+    return 'YES' if set(s1) & set(s2) else 'NO'
+
+
+print(two_strings(s1 ,s2))
