8d7fbc6618cd3d49a92302775efd5f02ae1eeed5
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-03-02 17:18:49 +0000

    retry ransom note

--- retry/ransomnote.py
+++ retry/ransomnote.py
@@ -11,4 +11,11 @@ from collections import Counter
 # A counter tool is provided to support convenient and rapid tallies
 # A Counter is a dict subclass for counting hashable objects. 
 # It is a collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
-print(Counter(magazine) + Counter(note))
+
+# In this case Counter keeps every character as a key and a count of each character as the value.
+print(Counter(note) - Counter(magazine))
+
+if Counter(note) - Counter(magazine) == { }:
+   print('Yes') 
+else:
+   print('No')
