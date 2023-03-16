73eb9700125f08864268ae85fda0e0ac9fb9deb7
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-03-16 11:00:11 +0000

    substrings

--- prep/substrings.py
+++ prep/substrings.py
@@ -1,6 +1,7 @@
 def findSubstring(st, subst):
     count = 0
     for i in range(0, len(st)):
+        # .startswith() returns True if the string starts with the specified prefix
         if st[i:].startswith(subst):
             count+=0
     return count 
